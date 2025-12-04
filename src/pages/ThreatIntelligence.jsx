1fe3bb4fc28e516bdea4ca31c61b8a865a7ba345
Moussa Yaya <yayamoussa2050@gmail.com>
2025-12-04 00:42:47 +0000

    File changes

--- src/pages/ThreatIntelligence.jsx
+++ src/pages/ThreatIntelligence.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
