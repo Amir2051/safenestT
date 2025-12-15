201ab18097bed13bb153ff1cdf39d794db743979
Moussa Yaya <yayamoussa2050@gmail.com>
2025-12-15 22:54:02 +0000

    File changes

--- src/components/investigation/CaseDetailDialog.jsx
+++ src/components/investigation/CaseDetailDialog.jsx
@@ -13,7 +13,7 @@ import {
   X, FileText, Clock, User, DollarSign, Shield, Upload, Plus, 
   MessageSquare, ExternalLink, Calendar, AlertCircle, Database, Building2,
   Edit, Save, Phone, Mail, MapPin, TrendingUp, Network, Sparkles, RefreshCw,
-  Eye, EyeOff
+  Eye, EyeOff, CheckCircle
 } from "lucide-react";
 import InvestigationNotes from "./InvestigationNotes.jsx";
 import RecommendedAgencies from "./RecommendedAgencies.jsx";
