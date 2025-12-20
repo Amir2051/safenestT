fdec8c419e6d86db00bd40374bc68f046dceae35
Moussa Yaya <yayamoussa2050@gmail.com>
2025-12-20 16:31:49 +0000

    File changes

--- functions/debugUserCases.ts
+++ functions/debugUserCases.ts
@@ -3,18 +3,26 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
 Deno.serve(async (req) => {
     try {
         const base44 = createClientFromRequest(req);
-        const email = "Dhgtrucking@gmail.com";
         
-        let user = null;
-        try {
-            const users = await base44.asServiceRole.entities.User.filter({ email: email });
-            if (users && users.length > 0) {
-                user = users[0];
-            }
-        } catch (e) {}
-
+        // Search cases created +/- 5 days from user creation (Nov 23)
+        const startDate = "2025-11-18T00:00:00.000Z";
+        const endDate = "2025-11-28T23:59:59.999Z";
+        
+        const allCases = await base44.asServiceRole.entities.MyCase.list('-created_date', 1000);
+        
+        const matches = allCases.filter(c => {
+            return c.created_date >= startDate && c.created_date <= endDate;
+        });
+        
         return Response.json({
-            user: user
+            count: matches.length,
+            cases: matches.map(c => ({
+                id: c.id,
+                created_date: c.created_date,
+                client_name: c.client_name,
+                client_email: c.client_email,
+                phone: c.phone_number
+            }))
         });
 
     } catch (error) {
